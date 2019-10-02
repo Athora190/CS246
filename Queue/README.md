e42f69e0723b8402cf466f1f546d85aa47c48f7f
Keeper9 <jermaine.n.reid@gmail.com>
2019-10-02 16:35:04 -0400

    Queue Data Structure

---
+++ Queue/README.md
@@ -0,0 +1,18 @@
+### Data Structure: 
++ Queue
+### Storage Method: 
++ Singly Linked Node
+### Key Methods:
++ Enqueue()  
+	+ Complexity: O(1)
++ Dequeue()
+	+ Complexity: O(1)
++ IsEmpty()
+	+ Complexity: O(1)
++ Peek()
+	+ Complexity: O(1)
+
+### Description: 
++ It is a data structure container that 
+removes items based on the FIFO 
+(First-In-First-Out) principle.  I
---
+++ Queue/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <ctime>
+#include "Queue.h"
+using namespace ds;
+
+int main()
+{
+	srand(time(NULL));
+	ds::Queue<int> queue;
+	std::string output, input;
+	int value;
+
+	input = "{";
+	for(int i = 0;i < 10;i += 1)
+	{
+		value = rand() % 24 + 5;
+		queue.Enqueue(value);
+		input += std::to_string(value) + ",";
+	}
+	input[input.length()-1] = '}';
+
+	std::cout << "Inputs: " << input << "\n";
+
+	output = "{";
+	while(!queue.IsEmpty())
+	{
+		output += queue.ToString() + ",";
+		queue.Dequeue();
+	}
+	output[output.length()-1] = '}';
+	
+	std::cout << "Outputs: " << output << "\n";
+ 
+	return 0;
+}
